044c11cb7907e752d7efebcc75f2bc415e87cf63
unknown <achetyre@gmail.com>
2021-01-15 01:22:47 +0300

    map update

--- overviewerConfig.js
+++ overviewerConfig.js
@@ -27,7 +27,7 @@ var overviewerConfig = {
     ],
     "map": {
         "debug": false,
-        "cacheTag": "1610274015",
+        "cacheTag": "1610657740",
         "north_direction": "lower-left",
         "controls": {
             "pan": true,
@@ -49,7 +49,7 @@ var overviewerConfig = {
             "base": "",
             "bgcolor": "#1a1a1a",
             "world": "world",
-            "last_rendertime": 1610273220,
+            "last_rendertime": 1610379688,
             "imgextension": "png",
             "isOverlay": false,
             "poititle": "Markers",
